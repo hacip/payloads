080c2680de2911fce0cd0b1b6964c45a0107cee0
hacip <mert.hacipoglu@gmail.com>
2020-10-02 10:51:25 -0400

    Update autons.ps1

--- autons.ps1
+++ autons.ps1
@@ -1,8 +1,8 @@
  ##Auto netsparker
 
-$nsExecPath="C:\Program Files (x86)\Netsparker\Netsparker.exe"
+$nsExecPath="C:\Program Files (x86)\aaa\Netsparker.exe"
 $reportTemplate = "Vulnerabilities List (XML)"
-$reportPath= "C:\Users\admin\Desktop\fallsviewcasino\reports\"
+$reportPath= "C:\Users\admin\Desktop\aaa\reports\"
 foreach ($url in get-content urllist.txt){
 $filename=$url.split('//')[2].replace(':','_')
 $report = $reportPath+$filename
